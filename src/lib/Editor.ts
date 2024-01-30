864f4d0938cc18bff80d074b82d52db5062536bc
GaspardCulis <gasdev.lp@gmail.com>
2024-01-30 22:19:51 +0100

    Created Editor singleton

---
+++ src/lib/Editor.ts
@@ -0,0 +1,40 @@
+import { EventEmitter } from "events";
+
+declare interface EditorVariable<T> {
+  on(event: "updated", listener: (new_value: T) => void): this;
+}
+
+class EditorVariable<T> extends EventEmitter {
+  constructor(private value: T) {
+    super();
+  }
+
+  get(): T {
+    return this.value;
+  }
+
+  set(new_value: T) {
+    this.value = new_value;
+    this.emit("updated", new_value);
+  }
+}
+
+export default class Editor {
+  private static _instance: Editor;
+
+  readonly variables = {
+    "title.top": new EditorVariable("Minecraft"),
+    "title.middle": new EditorVariable("Update"),
+    "title.bottom": new EditorVariable("Title Editor"),
+  };
+
+  private constructor() {}
+
+  static get(): Editor {
+    if (!Editor._instance) {
+      Editor._instance = new Editor();
+    }
+
+    return Editor._instance;
+  }
+}
