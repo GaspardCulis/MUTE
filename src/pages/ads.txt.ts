9921e259ddad4202fa1c45780a004b2f8cfb432b
GaspardCulis <gasdev.lp@gmail.com>
2024-02-11 11:20:47 +0100

    ads.txt: Added page

---
+++ src/pages/ads.txt.ts
@@ -0,0 +1,7 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async () => {
+  return new Response(
+    `google.com, pub-6558008609014707, DIRECT, f08c47fec0942fa0`,
+  );
+};
