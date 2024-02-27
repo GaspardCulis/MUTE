2996086566b0bd7898568011cdfaa1d499412a2e
GaspardCulis <gasdev.lp@gmail.com>
2024-02-27 21:20:39 +0100

    Create AdvancementOverlay.astro with associated css and env.d.ts declarations

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,14 @@
 type Vec3 = [number, number, number];
 type Vec4 = [number, number, number, number];
 type StyledText = { font: string; text: { text: string; texture: string }[] };
+
+type AdvancementParams = {
+  title: string;
+  icon: string | URL;
+  children?: HTMLElement[];
+};
+
+interface Window {
+  pushAdvancement(id: string, params: AdvancementParams): void;
+  popAdvancement(id: string): void;
+}
