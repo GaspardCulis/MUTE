43f94aca18b1fde4d27d94972c8c90003d5ef534
GaspardCulis <gasdev.lp@gmail.com>
2024-03-02 13:44:24 +0100

    robot.txt + ads.txt: Now using typescript routes

--- src/pages/ads.txt.ts
+++ src/pages/ads.txt.ts
@@ -3,5 +3,10 @@ import type { APIRoute } from "astro";
 export const GET: APIRoute = async () => {
   return new Response(
     `google.com, pub-6558008609014707, DIRECT, f08c47fec0942fa0`,
+    {
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8",
+      },
+    },
   );
 };
