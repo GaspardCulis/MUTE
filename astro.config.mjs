fe94f3cc4475948f64a1dd49245c149a116152fb
GaspardCulis <gaspard@gasdev.fr>
2024-05-13 09:25:11 +0200

    feat(astro.config): Configured i18n plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,27 @@
 import { defineConfig } from "astro/config";
-
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import sitemap from "@astrojs/sitemap";
+
+import { locales, defaultLocale } from "./src/i18n/ui";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.titlemc.app",
-  integrations: [sitemap()],
+  trailingSlash: "always",
+  build: {
+    format: "directory",
+  },
+  integrations: [
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale }),
+    }),
+  ],
 });
