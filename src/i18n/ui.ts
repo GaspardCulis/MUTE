869b1e276a21f297fee560e01c36e56972add02c
GaspardCulis <gaspard@gasdev.fr>
2024-05-13 13:03:05 +0200

    feat(i18n): Create ui.ts with translations definitions

---
+++ src/i18n/ui.ts
@@ -0,0 +1,33 @@
+export const defaultLocale = "en";
+export const locales = {
+  en: "en-US",
+  fr: "fr-FR",
+};
+
+export const ui = {
+  en: {
+    "head.title": "Minecraft Update Title Editor",
+    "head.description":
+      "The best tool to create and generate your own 3D Minecraft update logos with custom fonts and textures, looking just like the official ones.",
+
+    "editor.top": "Top",
+    "editor.middle": "Middle",
+    "editor.bottom": "Bottom",
+    "editor.save": "Save",
+    "editor.eula.agree":
+      "I agree to [Mojang's EULA](https://www.minecraft.net/en-us/eula)",
+    "editor.fonts": "Fonts",
+    "editor.textures": "Textures",
+
+    "footer.text":
+      "2024 JaajCorp, All rights reserved. We are not affiliate with Mojang.",
+    "footer.links.privacy": "Privacy Policy",
+
+    "footer.cookies": "This website uses cookies!",
+    "footer.cookies.accept": "Accept",
+  },
+  fr: {
+    "nav.home": "Accueil",
+    "nav.about": "À propos",
+  },
+} as const;
